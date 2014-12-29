ffd771f7d5ae74ded5c53a2344fca912892ada6c
Marco <Marco@Marco-PC>
2014-12-29 15:48:44 +0100

    Petit bug corrigé

--- Projet/application/views/index.php
+++ Projet/application/views/index.php
@@ -11,9 +11,11 @@ and open the template in the editor.
         <link rel="stylesheet" type="text/css" href="<?php echo css_url('bootstrap'); ?>" />
         <link rel="stylesheet" type="text/css" href="<?php echo css_url('stylesheet');?>" />
 	<link rel="icon" href="../../assets/img/favicon.jpg">
+        <style type="text/css">
+		li.container {list-style: none; display: inline}
+	</style>
     </head>
     <body>
-        <div class="container">
             <!--<div class="row">
                 <nav>
                     <p>Rechercher par : </p>
@@ -30,55 +32,52 @@ and open the template in the editor.
                     <p>Connexion</p>
                 </nav>
             </div>-->
-			<ul id="main_menu">
-				<li><a href="#">Orchestre</a>
-					<ul>
-						<li><a href="orchestra.php">Tous les orchestres</a></li>
-						<div id="menu_bandmaster">
-							<li>
-								<a href="bandmaster.php">Tous les chefs d'orchestre</a>
-								
-									<li><a href="A">A</a></li>
-									<li><a href="#">.</a></li>
-									<li><a href="#">.</a></li>
-									<li><a href="#">.</a></li>
-									<li><a href="Z">Z</a></li>
-								
-							</li>
-						</div>
-					</ul>
-				</li>
-				<li><a href="#">Rechercher par</a>
-					<ul>
-						<li><a href="#">Compositeur</a></li>
-						<li><a href="#">Date de parution</a></li>
-						<li><a href="#">Genre</a></li>
-						<li><a href="#">Instrument</a></li>
-						<li><a href="#">Interprète</a></li>
-					</ul>
-				</li>
-				<li><a href="#">Albums</a>
-					<ul id="menu_albums">
-						<li><a href="#">Tous les albums</a></li>
+            <ul id="main_menu">
+		<li><a href="#">Orchestre</a>
+			<ul>
+				<li><a href="orchestra.php">Tous les orchestres</a></li>
+				<li class="container">
+					<div id="menu_bandmaster">
+						<a href="bandmaster.php">Tous les chefs d'orchestre</a>
 						<ul>
-							<li><a href="#">A</a></li>
+							<li><a href="A">A</a></li>
 							<li><a href="#">.</a></li>
 							<li><a href="#">.</a></li>
 							<li><a href="#">.</a></li>
-							<li><a href="#">Z</a></li>
+							<li><a href="Z">Z</a></li>
 						</ul>
-					</ul>
+					</div>
 				</li>
-				<li>
-					<a href="registration.php">S'inscrire</a>
-				</li>
-				<li>
-					<a href="connection.php">Connexion</a>
-				</li>
-			</ul>			
-            <img src="../../assets/img/background.jpg" alt="First slide">
-			</img>
-            <?php echo base_url(); ?>
-        </div>
+			</ul>
+		</li>
+		<li><a href="#">Rechercher par</a>
+			<ul>
+				<li><a href="#">Compositeur</a></li>
+				<li><a href="#">Date de parution</a></li>
+				<li><a href="#">Genre</a></li>
+				<li><a href="#">Instrument</a></li>
+				<li><a href="#">Interprète</a></li>
+			</ul>
+		</li>
+		<li><a href="#">Albums</a>
+			<ul id="menu_albums">
+				<li><a href="#">Tous les albums</a></li>
+				
+					<li><a href="#">A</a></li>
+					<li><a href="#">.</a></li>
+					<li><a href="#">.</a></li>
+					<li><a href="#">.</a></li>
+					<li><a href="#">Z</a></li>
+				
+			</ul>
+		</li>
+		<li>
+			<a href="registration.php">S'inscrire</a>
+		</li>
+		<li>
+			<a href="connection.php">Connexion</a>
+		</li>
+	</ul>			
+	<img src="background.jpg" alt="First slide">
     </body>
 </html>

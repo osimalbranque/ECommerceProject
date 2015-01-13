0a69bc8070334a0e88cbcbfb6b7a02152e803ba2
Marco <Marco@Marco-PC>
2015-01-14 00:54:02 +0100

    CSS moins pire

--- Projet/application/views/General/footer.php
+++ Projet/application/views/General/footer.php
@@ -1,6 +1,6 @@
 <footer>
         <p>
-            © 2015 IUT</span> Informatique de Bordeaux</strong><br>
+            © 2015 IUT<strong> Informatique de Bordeaux</strong><br>
             Site web réalisé par Marco Lugustiano et Osiris Malbranque
 	</p>  
 </footer>
